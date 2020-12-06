3130a42091197fac87cfd47420d475274df338d5
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-12-06 16:01:06 +0100

    Add meta viewport tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
 </head>
     <body style="margin:0;">
-        <iframe src="{{ $store_url }}" style="height:100vh; width:100%; border: none;">
+        <iframe id="storeFrame" name="storeFrame" src="{{ $store_url }}" style="height:100vh; width:100%; border: none;">
     </body>
 </html>
